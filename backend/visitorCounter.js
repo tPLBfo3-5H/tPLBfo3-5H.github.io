973354d9badfb3c8efc7759f32c6e8bf1f2bc135
Adolphus Noel <alnjr08@gmail.com>
2022-11-01 16:06:52 -0500

    added visitorCounter.js

---
+++ backend/visitorCounter.js
@@ -0,0 +1,23 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    getVisitCount();
+  });
+  
+  
+  const functionApi = 'https://azure-function-resume.azurewebsites.net/api/HttpTrigger1?code=90SQnCIx4EIpPnyjOmLYNz8cGHopCUlSU4oo9Lb3zz71AzFuX9xQiA==';
+  
+  const getVisitCount = () => {
+    let count = 7;
+    fetch(functionApi)
+      .then(response => {
+        return response.json()
+      })
+      .then(response => {
+        count = response.VisitorCount;
+        console.log("Fetch to function succeeded.");
+        document.getElementById('counter').innerText = count;
+      }).catch(function (error) {
+        console.log(error);
+      });
+    return count;
+  }
+  
